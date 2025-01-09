b1d57b08e64ad26919c717135faf35f86791af8b
Nguyễn Văn Thắng <34498737+nv-thang@users.noreply.github.com>
2025-01-09 21:54:04 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import math
+
+def solve_quadratic(a, b, c):
+    """
+    Giải phương trình bậc 2: ax^2 + bx + c = 0
+    Trả về:
+        - (x1, x2): nếu có 2 nghiệm phân biệt
+        - (x1, None): nếu có nghiệm kép
+        - None: nếu vô nghiệm
+    """
+    # Viết mã của bạn tại đây
+    pass
